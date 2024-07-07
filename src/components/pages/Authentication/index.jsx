9e087808e7eac81f080f1bb649f3c5ff6c0eec95
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2024-07-07 20:40:09 +0500

    created form with schema for login

--- src/components/pages/Authentication/index.jsx
+++ src/components/pages/Authentication/index.jsx
@@ -1,103 +1,166 @@
-import React, {  useState } from "react";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
-import { login } from "../../imagepath";
-import { Link } from "react-router-dom";
-import FeatherIcon from "feather-icons-react";
-import { Eye, EyeOff } from "react-feather/dist";
+import React, { useState } from 'react';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import { login } from '../../imagepath';
+import { Link, useHistory } from 'react-router-dom';
+import { Eye, EyeOff } from 'react-feather/dist';
+import { FormProvider } from '../../HookForm';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 const Login = () => {
+  const history = useHistory();
 
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [password, setPassword] = useState('');
 
   const togglePasswordVisibility = () => {
     setPasswordVisible(!passwordVisible);
   };
 
+  /*
+  Role Based Authentication
 
+  const [(activeStep, setActiveStep)] = useState(0);
+  const createLoginSchema = [
+    Yup.object().shape({
+      role: Yup.string().required(),
+    }),
+    Yup.object().shape({
+      email: Yup.email().required(),
+    }),
+  ];
+
+  const currentLoginSchema = createLoginSchema[activeStep];
+  */
+
+  const LoginSchema = Yup.object().shape({
+    username: Yup.string().required('Username is required'),
+    password: Yup.string().required('Password is required'),
+  });
+
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = methods;
+
+  const handleLogin = (data) => {
+    const { username, password } = data;
+    if (username === 'admin') {
+      history.replace('/admindashboard');
+    } else if (username === 'teacher') {
+      history.replace('/teacherdashboard');
+    }
+  };
+  console.log(errors);
   return (
     <>
-      <div className="main-wrapper login-body">
-        <div className="login-wrapper">
-          <div className="container">
-            <div className="loginbox">
-              <div className="login-left">
-                <img className="img-fluid" src={login} alt="Logo" />
+      <div className='main-wrapper login-body'>
+        <div className='login-wrapper'>
+          <div className='container'>
+            <div className='loginbox'>
+              <div className='login-left'>
+                <img className='img-fluid' src={login} alt='Logo' />
               </div>
-              <div className="login-right">
-                <div className="login-right-wrap">
+              <div className='login-right'>
+                <div className='login-right-wrap'>
                   <h1>Welcome to Preskool</h1>
-                  <p className="account-subtitle">
-                    Need an account? <Link to="/register">Sign Up</Link>
+                  <p className='account-subtitle'>
+                    Need an account? <Link to='/register'>Sign Up</Link>
                   </p>
                   <h2>Sign in</h2>
                   {/* Form */}
-                  <form action="./admindashboard">
-                    <div className="form-group">
+                  <FormProvider
+                    methods={methods}
+                    onSubmit={handleSubmit(handleLogin)}
+                  >
+                    <div className='form-group'>
                       <label>
-                        Username <span className="login-danger">*</span>
+                        Username <span className='login-danger'>*</span>
                       </label>
-                      <input className="form-control" type="text" />
-                      <span className="profile-views">
-                        <i className="fas fa-user-circle" />
-                      </span>
-                    </div>
-                    <div className="form-group">
-                      <label>
-                        Password <span className="login-danger">*</span>
-                      </label>
-                     
                       <input
-                      type={passwordVisible ? '' : 'password'} 
-                      className="form-control pass-input"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <span
-                      className="toggle-password"
-                      onClick={togglePasswordVisibility}
-                    >
-                      {passwordVisible ? <EyeOff className="react-feather-custom" /> : <Eye className="react-feather-custom" />}
-                    </span>
-              
-                     
+                        className='form-control'
+                        type='text'
+                        {...register('username')}
+                      />
+                      <span className='profile-views'>
+                        <i className='fas fa-user-circle' />
+                      </span>
+                      {errors.username && (
+                        <div className='text-danger'>
+                          {errors.username.message}
+                        </div>
+                      )}
                     </div>
-                    <div className="forgotpass">
-                      <div className="remember-me">
-                        <label className="custom_check mr-2 mb-0 d-inline-flex remember-me">
-                          {" "}
+                    <div className='form-group'>
+                      <label>
+                        Password <span className='login-danger'>*</span>
+                      </label>
+
+                      <input
+                        type={passwordVisible ? '' : 'password'}
+                        className='form-control pass-input'
+                        {...register('password')}
+                      />
+                      <span
+                        className='toggle-password'
+                        onClick={togglePasswordVisibility}
+                      >
+                        {passwordVisible ? (
+                          <EyeOff className='react-feather-custom' size={16} />
+                        ) : (
+                          <Eye className='react-feather-custom' size={16} />
+                        )}
+                      </span>
+                      {errors.password && (
+                        <div className='text-danger'>
+                          {errors.password.message}
+                        </div>
+                      )}
+                    </div>
+                    <div className='forgotpass'>
+                      <div className='remember-me'>
+                        <label className='custom_check mr-2 mb-0 d-inline-flex remember-me'>
+                          {' '}
                           Remember me
-                          <input type="checkbox" name="radio" />
-                          <span className="checkmark" />
+                          <input type='checkbox' name='radio' />
+                          <span className='checkmark' />
                         </label>
                       </div>
-                      <Link to="/forgotpassword">Forgot Password?</Link>
+                      <Link to='/forgotpassword'>Forgot Password?</Link>
                     </div>
-                    <div className="form-group">
-                      <button className="btn btn-primary btn-block" type="submit">
+                    <div className='form-group'>
+                      <button
+                        className='btn btn-primary btn-block'
+                        type='submit'
+                      >
                         Login
                       </button>
                     </div>
-                  </form>
+                  </FormProvider>
                   {/* /Form */}
-                  <div className="login-or">
-                    <span className="or-line" />
-                    <span className="span-or">or</span>
+                  <div className='login-or'>
+                    <span className='or-line' />
+                    <span className='span-or'>or</span>
                   </div>
                   {/* Social Login */}
-                  <div className="social-login">
-                    <Link to="#">
-                      <i className="fab fa-google-plus-g" />
+                  <div className='social-login'>
+                    <Link to='#'>
+                      <i className='fab fa-google-plus-g' />
                     </Link>
-                    <Link to="#">
-                      <i className="fab fa-facebook-f" />
+                    <Link to='#'>
+                      <i className='fab fa-facebook-f' />
                     </Link>
-                    <Link to="#">
-                      <i className="fab fa-twitter" />
+                    <Link to='#'>
+                      <i className='fab fa-twitter' />
                     </Link>
-                    <Link to="#">
-                      <i className="fab fa-linkedin-in" />
+                    <Link to='#'>
+                      <i className='fab fa-linkedin-in' />
                     </Link>
                   </div>
                 </div>
@@ -106,7 +169,6 @@ const Login = () => {
           </div>
         </div>
       </div>
-
     </>
   );
 };
