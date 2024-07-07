7ef49cccdf834c81118c029085bfb927d71aa6f9
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2024-07-07 20:39:38 +0500

    exporting FormProvider from index file

---
+++ src/components/HookForm/index.ts
@@ -0,0 +1,13 @@
+// export * from "./RHFCheckbox";
+// export * from "./RHFUpload";
+
+export { default as FormProvider } from './FormProvider';
+
+// export { default as RHFSwitch } from "./RHFSwitch";
+// export { default as RHFSelect } from "./RHFSelect";
+// export { default as RHFEditor } from "./RHFEditor";
+// export { default as AntDTextField } from './AntDTextField';
+// export { default as RHFRadioGroup } from "./RHFRadioGroup";
+// export { default as RHFPhoneNumber } from "./RHFPhoneNumber";
+// export { default as RHFDatePicker } from "./RHFDatePicker";
+// export { default as RHFAutocomplete } from "./RHFAutocomplete";
