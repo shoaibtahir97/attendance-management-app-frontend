17a3129d8d162dbd5bb5adea01c4cbf23e39711d
Muhammad Shoaib Tahir <stahir@mydost.ai>
2024-07-12 23:38:33 +0500

    integated mui with react hook form alongwith formprovider

--- src/components/HookForm/FormProvider.jsx
+++ src/components/HookForm/FormProvider.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { FormProvider as Form } from 'react-hook-form';
+import React, { ReactNode } from "react";
+import { FormProvider as Form, UseFormReturn } from "react-hook-form";
 
 export default function FormProvider({ children, onSubmit, methods }) {
   return (
--- src/components/HookForm/index.ts
+++ src/components/HookForm/index.ts
@@ -1,7 +1,7 @@
 // export * from "./RHFCheckbox";
 // export * from "./RHFUpload";
 
-export { default as FormProvider } from './FormProvider';
+export { default as FormProvider } from "./FormProvider";
 
 // export { default as RHFSwitch } from "./RHFSwitch";
 // export { default as RHFSelect } from "./RHFSelect";
@@ -9,5 +9,6 @@ export { default as FormProvider } from './FormProvider';
 // export { default as AntDTextField } from './AntDTextField';
 // export { default as RHFRadioGroup } from "./RHFRadioGroup";
 // export { default as RHFPhoneNumber } from "./RHFPhoneNumber";
-export { default as RHFDatePicker } from './RHFDatePicker';
-// export { default as RHFAutocomplete } from "./RHFAutocomplete";
+export { default as RHFDatePicker } from "./RHFDatePicker";
+export { default as RHFDateCalendar } from "./RHFDateCalendar";
+export { default as RHFAutocomplete } from "./RHFAutocomplete";
