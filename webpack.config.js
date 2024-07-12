588683fa01a4d05cda4d2e6ac2ea1e3adac58634
Muhammad Shoaib Tahir <stahir@mydost.ai>
2024-07-13 01:11:31 +0500

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,165 +11,148 @@ const opn = require("opn"); // Import the 'opn' package
 
 const publicPath = "/"; // To run this file in local
 // const publicPath = "/react/"; // To build the file
-module.exports = {
-  mode: "development",
-  entry: path.join(__dirname, "src", "index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-  },
-  devServer: {
-    port: 3001,
-    historyApiFallback: true,
-    onAfterSetupMiddleware() {
-      opn(`http://localhost:${this.port}/login`);
+module.exports = (env, args) => {
+  const mode = args.mode || "development";
+  return {
+    mode,
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
     },
-  },
-  // devServer: {
-  //   static: {
-  //     directory: path.join(__dirname, "public/"),
-  //   },
-  //   port: 3000,
-  //   historyApiFallback: true,
-  //   open: true, // Open the browser after the dev server is up and running
-  // },
-  // module.exports = {
-  //   mode: "development",
-  //   entry: path.join(__dirname, "src", "index.js"),
-  //   output: {
-  //     path:path.resolve(__dirname, "dist"),
-  //     // publicPath: publicPath, // base path where referenced files will be look for
-  //   },
-  //
-  //  devServer: {
-  //   static: {
-  //     directory: path.join(__dirname, "public/"),
-  //   },
-  //   port: 3002,
-  //   historyApiFallback: true,
-  //   onAfterSetupMiddleware() {
-  //     // Open the browser after the dev server is up and running
-  //     // const open = require('open');
-  //     // opn(`http://localhost:${this.port}`);
-  //     opn(`http://localhost:${devServer.options.port}`);
+    devServer: {
+      port: 3001,
+      historyApiFallback: true,
+      onAfterSetupMiddleware() {
+        opn(`http://localhost:${this.port}/login`);
+      },
+    },
+    performance: {
+      hints: false,
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+    },
+    // devServer: {
+    //   static: {
+    //     directory: path.join(__dirname, "public/"),
+    //   },
+    //   port: 3000,
+    //   historyApiFallback: true,
+    //   open: true, // Open the browser after the dev server is up and running
+    // },
+    // module.exports = {
+    //   mode: "development",
+    //   entry: path.join(__dirname, "src", "index.js"),
+    //   output: {
+    //     path:path.resolve(__dirname, "dist"),
+    //     // publicPath: publicPath, // base path where referenced files will be look for
+    //   },
+    //
+    //  devServer: {
+    //   static: {
+    //     directory: path.join(__dirname, "public/"),
+    //   },
+    //   port: 3002,
+    //   historyApiFallback: true,
+    //   onAfterSetupMiddleware() {
+    //     // Open the browser after the dev server is up and running
+    //     // const open = require('open');
+    //     // opn(`http://localhost:${this.port}`);
+    //     opn(`http://localhost:${devServer.options.port}`);
 
-  //     // opn(`http://localhost:${this.port}/template3/`);
-  //   },
-  // },
+    //     // opn(`http://localhost:${this.port}/template3/`);
+    //   },
+    // },
 
-  externals: {
-    // global app config object
-    config: JSON.stringify({
-      apiUrl: "",
-      // publicPath : '/'   ,
-      publicPath: publicPath,
-    }),
-  },
-  output: {
-    filename: "js/[name].bundle.js",
-    path: path.resolve(__dirname, "dist"), // base path where to send compiled assets
-    publicPath: publicPath, // base path where referenced files will be look for
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+    externals: {
+      // global app config object
+      config: JSON.stringify({
+        apiUrl: "",
+        // publicPath : '/'   ,
+        publicPath: publicPath,
+      }),
+    },
+    output: {
+      filename: "js/[name].bundle.js",
+      path: path.resolve(__dirname, "dist"), // base path where to send compiled assets
+      publicPath: publicPath, // base path where referenced files will be look for
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js", ".jsx"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
           },
         },
-      },
 
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        // config for sass compilation
-        test: /\.scss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          "css-loader",
-          {
-            loader: "sass-loader",
-          },
-        ],
-      },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          // config for sass compilation
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            "css-loader",
+            {
+              loader: "sass-loader",
+            },
+          ],
+        },
 
-      {
-        test: /\.(jpg|png|svg|gif)$/,
-        type: "asset/resource",
-      },
-    ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": "development",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-
-    new HtmlWebpackPlugin({
-      title: "Hot Module Replacement",
-      template: "./public/index.html",
-      filename: "./index.html",
-      favicon: "./public/favicon.png",
-    }),
-    new MiniCssExtractPlugin({
-      // plugin for controlling how compiled css will be outputted and named
-      filename: "css/[name].css",
-      chunkFilename: "css/[id].css",
-    }),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        "css/*.*",
-        "js/*.*",
-        "fonts/*.*",
-        "images/*.*",
+        {
+          test: /\.(jpg|png|svg|gif)$/,
+          type: "asset/resource",
+        },
       ],
-    }),
-  ],
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify("development"),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-
-    new HtmlWebpackPlugin({
-      title: "Hot Module Replacement",
-      template: "./public/index.html",
-      filename: "./index.html",
-      favicon: "./public/favicon.png",
-    }),
-    new MiniCssExtractPlugin({
-      // plugin for controlling how compiled css will be outputted and named
-      filename: "css/[name].css",
-      chunkFilename: "css/[id].css",
-    }),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        "css/*.*",
-        "js/*.*",
-        "fonts/*.*",
-        "images/*.*",
-      ],
-    }),
-    new webpack.ProvidePlugin({
-      //To automatically load jquery
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery",
-    }),
-  ],
-  optimization: {
-    splitChunks: {
-      chunks: "all",
     },
-  },
+
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(mode),
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+
+      new HtmlWebpackPlugin({
+        title: "Hot Module Replacement",
+        template: "./public/index.html",
+        filename: "./index.html",
+        favicon: "./public/favicon.png",
+      }),
+      new MiniCssExtractPlugin({
+        // plugin for controlling how compiled css will be outputted and named
+        filename: "css/[name].css",
+        chunkFilename: "css/[id].css",
+      }),
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: [
+          "css/*.*",
+          "js/*.*",
+          "fonts/*.*",
+          "images/*.*",
+        ],
+      }),
+      new webpack.ProvidePlugin({
+        //To automatically load jquery
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery",
+      }),
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+    },
+  };
 };
