ca2097e5c04747909150e0f23bb2997a023aab12
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2024-07-14 11:51:41 +0500

    created reports page

---
+++ src/components/pages/Reports/Reports.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Reports = () => {
+  return (
+    <div className='content container-fluid'>
+      <div className='page-header'>
+        <div className='row'>
+          <div className='col-sm-12'>
+            <div className='page-sub-header'>
+              <h3 className='page-title'>Reports</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reports;
