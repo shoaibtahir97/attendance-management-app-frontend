5a0dd1816c077a15c848c524a4b88e9b02f0967c
Muhammad Shoaib Tahir <stahir@mydost.ai>
2024-08-06 20:43:15 +0500

    added calendar page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,44 +1,45 @@
-import React from 'react';
+import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   Routes,
-} from 'react-router-dom';
-import AuthLayout from '../components/Layouts/AuthLayout.t';
-import DashboardLayout from '../components/Layouts/DashboardLayout';
-import AttendancePage from '../components/pages/Attendance';
-import Login from '../components/pages/Authentication';
-import Register from '../components/pages/Authentication/Register';
-import AdminDashboard from '../components/pages/Dashboard/AdminDashboard';
-import TeacherDashboard from '../components/pages/Dashboard/TeacherDashboard';
-import StudentsDashboard from '../components/pages/Dashboard/StudentsDashboard';
-import Students from '../components/pages/Students/StudentsList';
-import StudentsView from '../components/pages/Students/StudentsView';
-import AddStudent from '../components/pages/Students/AddStudent';
-import EditStudent from '../components/pages/Students/EditStudent';
-import TeachersList from '../components/pages/Teachers/TeachersList';
-import TeachersProfile from '../components/pages/Teachers/TeachersProfile';
-import TeachersAdd from '../components/pages/Teachers/TeachersAdd';
-import TeachersEdit from '../components/pages/Teachers/TeachersEdit';
-import DepartmentList from '../components/pages/Department/DepartmentList';
-import AddDepartment from '../components/pages/Department/AddDepartment';
-import EditDepartment from '../components/pages/Department/EditDepartment';
-import SubjectList from '../components/pages/Subject/SubjectList';
-import AddSubject from '../components/pages/Subject/AddSubject';
-import EditSubject from '../components/pages/Subject/EditSubject';
-import ForgotPassword from '../components/pages/Authentication/ForgotPassword';
-import ProtectedRoute from './ProtectedRoute';
-import { PATH_AUTH, PATH_DASHBOARD } from './paths';
-import Reports from '../components/pages/Reports/Reports';
-import StudentReports from '../components/pages/Reports/StudentReports';
-import AttendanceReports from '../components/pages/Reports/AttendanceReports';
+} from "react-router-dom";
+import AuthLayout from "../components/Layouts/AuthLayout.t";
+import DashboardLayout from "../components/Layouts/DashboardLayout";
+import AttendancePage from "../components/pages/Attendance";
+import Login from "../components/pages/Authentication";
+import Register from "../components/pages/Authentication/Register";
+import AdminDashboard from "../components/pages/Dashboard/AdminDashboard";
+import TeacherDashboard from "../components/pages/Dashboard/TeacherDashboard";
+import StudentsDashboard from "../components/pages/Dashboard/StudentsDashboard";
+import Students from "../components/pages/Students/StudentsList";
+import StudentsView from "../components/pages/Students/StudentsView";
+import AddStudent from "../components/pages/Students/AddStudent";
+import EditStudent from "../components/pages/Students/EditStudent";
+import TeachersList from "../components/pages/Teachers/TeachersList";
+import TeachersProfile from "../components/pages/Teachers/TeachersProfile";
+import TeachersAdd from "../components/pages/Teachers/TeachersAdd";
+import TeachersEdit from "../components/pages/Teachers/TeachersEdit";
+import DepartmentList from "../components/pages/Department/DepartmentList";
+import AddDepartment from "../components/pages/Department/AddDepartment";
+import EditDepartment from "../components/pages/Department/EditDepartment";
+import SubjectList from "../components/pages/Subject/SubjectList";
+import AddSubject from "../components/pages/Subject/AddSubject";
+import EditSubject from "../components/pages/Subject/EditSubject";
+import ForgotPassword from "../components/pages/Authentication/ForgotPassword";
+import ProtectedRoute from "./ProtectedRoute";
+import { PATH_AUTH, PATH_DASHBOARD } from "./paths";
+import Reports from "../components/pages/Reports/Reports";
+import StudentReports from "../components/pages/Reports/StudentReports";
+import AttendanceReports from "../components/pages/Reports/AttendanceReports";
+import CalendarPage from "../components/pages/Calendar";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path='/' element={<AuthLayout />}>
-        <Route path='/login' element={<Login />} />
+      <Route path="/" element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
         <Route path={PATH_AUTH.register} element={<Register />} />
         <Route
           index={true}
@@ -46,8 +47,8 @@ const router = createBrowserRouter(
           element={<ForgotPassword />}
         />
       </Route>
-      <Route path='' element={<ProtectedRoute />}>
-        <Route path='/dashboard' element={<DashboardLayout />}>
+      <Route path="" element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<DashboardLayout />}>
           {/* Dashboard */}
           <Route
             path={PATH_DASHBOARD.adminDashboard}
@@ -129,13 +130,17 @@ const router = createBrowserRouter(
         <Route path='/dashboard/invoicelist' element={<InvoiceList />} /> */}
 
           {/* Attendance */}
-          <Route path='/dashboard/attendance' element={<AttendancePage />} />
+          <Route path="/dashboard/attendance" element={<AttendancePage />} />
 
           {/* Reports */}
           <Route
             path={PATH_DASHBOARD.attendanceReports}
             element={<AttendanceReports />}
           />
+
+          {/* Calendar */}
+          <Route path={PATH_DASHBOARD.calendar} element={<CalendarPage />} />
+
           {/* <Route
             path='/dashboard/reports/teachers'
             element={<TeacherReports />}
