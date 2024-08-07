c35fb22dcd2e4e0e90630026688a2ea4a43da1fd
Muhammad Shoaib Tahir <stahir@mydost.ai>
2024-08-07 19:06:42 +0500

    added fullcalendar component

--- src/components/pages/Calendar/index.js
+++ src/components/pages/Calendar/index.js
@@ -1,7 +1,31 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 import React from "react";
+import { Box } from "@mui/material";
+
+const events = [{ title: "Meeting", start: new Date() }];
 
 const CalendarPage = () => {
-  return <div>CalendarPage</div>;
+  return (
+    <Box sx={{ p: 3 }}>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        weekends={false}
+        events={events}
+        eventContent={renderEventContent}
+      />
+    </Box>
+  );
 };
+
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  );
+}
 
 export default CalendarPage;
