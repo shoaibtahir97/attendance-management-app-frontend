623bcd2f33d0bbf04dab4af979ff11824f7bedcc
Muhammad Shoaib Tahir <stahir@mydost.ai>
2024-08-28 21:01:32 +0500

    created group and subject slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
 import authSliceReducer from './slices/authSlice';
+import subjectSliceReducer from './slices/subjectSlice';
+import groupSliceReducer from './slices/groupSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSliceReducer,
+    subjects: subjectSliceReducer,
+    groups: groupSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
