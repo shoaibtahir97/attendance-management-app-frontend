80be16c498566ae35612118541c507fea73156fd
Muhammad Shoaib Tahir <stahir@mydost.ai>
2024-09-01 13:38:41 +0500

    updated base url

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://attendance-management-app-backend-3kdw.onrender.com';
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://attendance-management-app-backend-3kdw.onrender.com';
+// const BASE_URL = 'http://localhost:5000';
 const STUDENTS_URL = '/api/v1/students';
 const USERS_URL = '/api/v1/users';
 const ATTENDANCE_URL = '/api/v1/attendance';
