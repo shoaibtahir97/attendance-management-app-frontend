f45afd32c68021bbed0f83db78fd981fd0a768d5
Muhammad Shoaib Tahir <stahir@mydost.ai>
2024-09-08 15:40:15 +0500

    logging out user if status code is unauthorized

--- src/redux/slices/apiSlices/apiSlice.js
+++ src/redux/slices/apiSlices/apiSlice.js
@@ -1,12 +1,23 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../../../constants';
+import { logout } from '../authSlice';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
 });
 
+const baseQueryWithAuth = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.status === 401) {
+    api.dispatch(logout());
+  }
+
+  return result;
+};
+
 export const apiSlice = createApi({
-  baseQuery,
+  baseQuery: baseQueryWithAuth,
   tagTypes: [
     'Student',
     'Teacher',
