edf78f8e37e36cfedd9e52d7a751fb44095e9032
Muhammad Shoaib Tahir <stahir@mydost.ai>
2025-03-24 08:24:52 +0500

    added loading state to button

--- src/redux/slices/apiSlices/apiSlice.js
+++ src/redux/slices/apiSlices/apiSlice.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../../../constants';
 import { logout } from '../authSlice';
-import { getDefaultMiddleware } from '@reduxjs/toolkit';
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
 });
@@ -12,7 +11,6 @@ const baseQueryWithAuth = async (args, api, extraOptions) => {
   if (result.error && result.error.status === 401) {
     api.dispatch(logout());
   }
-
   return result;
 };
 
